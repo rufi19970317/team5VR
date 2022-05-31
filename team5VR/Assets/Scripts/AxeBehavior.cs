8f5ba57405b05a193d44d8af0b7eea8cd54e4e1c
XerryJun <102278022+XerryJun@users.noreply.github.com>
2022-06-01 01:44:16 +0900

    장작통기능 추가

--- team5VR/Assets/Scripts/AxeBehavior.cs
+++ team5VR/Assets/Scripts/AxeBehavior.cs
@@ -11,10 +11,9 @@ public class AxeBehavior : MonoBehaviour
     
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.CompareTag("Wood")) {
+        if (collision.transform.CompareTag("Wood") && collision.transform.position.y > 1) {
             Instantiate(smallWood, new Vector3(collision.transform.position.x,collision.transform.position.y,collision.transform.position.z), collision.transform.rotation);
             Instantiate(smallWood, new Vector3(collision.transform.position.x, collision.transform.position.y, collision.transform.position.z+0.3f), collision.transform.rotation);
-
             Destroy(collision.transform.gameObject);
         }
     }
