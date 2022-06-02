80c57c1a17d1f977b75bce3ff32a47a22b31e49a
ChoiDongHo <goguma1000@ajou.ac.kr>
2022-06-03 07:07:24 +0900

    인벤토리완성(디자인 제외): 버킷과 연동되도록 구현

--- team5VR/Assets/Scripts/ItemInfo.cs
+++ team5VR/Assets/Scripts/ItemInfo.cs
@@ -9,10 +9,15 @@ public class ItemInfo : MonoBehaviour
     public bool inSlot = false;
     public Vector3 slotRotation = Vector3.zero;
     public Vector3 defauultSize;
+    public Vector3 offset;
+    public Vector3 slotSize;
     // Start is called before the first frame update
     void Start()
     {
-        defauultSize = transform.localScale;
+        if (!inSlot)
+        {
+            defauultSize = transform.localScale;
+        }
     }
 
     // Update is called once per frame
